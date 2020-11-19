69ae3781b13c5c957e555d76fd8148ca73b7c14b
jeff-dotcom <jeff.reyns.ja@gmail.com>
2020-11-19 10:12:11 +0000

    First commit

---
+++ app.py
@@ -0,0 +1,12 @@
+print('Prince is learning python')
+print('I am new to AWS')
+
+firstnumber = 20
+secondnumber = 30
+total = firstnumber + secondnumber
+print('total')
+print(total)
+third = 4
+fourth = 9
+product = third * fourth
+print(product)
